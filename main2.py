d5e20a3035af90dffe7ae147df162f5731ffcbe3
Erik Örjehag <erik@orjehag.se>
2023-01-28 15:23:10 +0100

    asd

--- main2.py
+++ main2.py
@@ -25,8 +25,8 @@ def create_link(p: tp.Optional[Link], m: float, l: float, q: float, inertia: str
     return Link(p=p, m=m, l=l, I=I, q=q)
 
 links = []
-links.append(create_link(p=None, m=1.0, l=1.0, q=0.0, inertia="slender-bar"))
-links.append(create_link(p=links[0], m=1.0, l=1.0, q=0.0, inertia="slender-bar"))
+links.append(create_link(p=None, m=2.0, l=3.0, q=0.0, inertia="slender-bar"))
+links.append(create_link(p=links[0], m=5.0, l=7.0, q=0.0, inertia="slender-bar"))
 
 def to_q(links: tp.List[Link]):
     q = sp.Matrix([
@@ -46,7 +46,6 @@ def Rz(q):
 
 def inertia_matrix(links: tp.List[Link]):
     q = to_q(links)
-    dq = q.diff(t)
     # TODO: Follow parent chaining and determine axis, add prismatic links
     R = [Rz(qi) for qi in np.cumsum(q)]
     # TODO: Follow parent chaining and determine axis, add prismatic links
@@ -70,8 +69,8 @@ def inertia_matrix(links: tp.List[Link]):
         0.0
     ])
     c1 = sp.Matrix([
-        links[0].l * sp.cos(q[0]) + links[1].l/2 * sp.cos(q[1]),
-        links[0].l * sp.cos(q[0]) + links[1].l/2 * sp.sin(q[1]),
+        links[0].l * sp.cos(q[0]) + links[1].l/2 * sp.cos(q[0] + q[1]),
+        links[0].l * sp.sin(q[0]) + links[1].l/2 * sp.sin(q[0] + q[1]),
         0.0
     ])
     # dc0 = c0.diff(t)
@@ -86,15 +85,52 @@ def inertia_matrix(links: tp.List[Link]):
         for i in range(len(links))
     ], sp.zeros(len(links)))
 
-    print("ASD")
-    # assert Dq.shape == (len(links), len(links)), "nxn"
-    # assert Dq.T = Dq, "symetric"
-    # assert Dq.is_positive_definite, "positive definite"
+    D.simplify()
 
-def kinetic_enegy(links: tp.List[Link]):
+    assert D.shape == (len(links), len(links)), "nxn"
+    assert D.is_symmetric(), "symetric"
+    #assert D.is_positive_definite, "positive definite"
+
+    return D
+
+def christoffel(D):
+    q = to_q(links)
+    for k in range(len(links)):
+        for i in range(len(links)):
+            for j in range(i, len(links)):
+                #print(f"{i=}, {j=}, {k=}")
+                c = 0.5 * (D[k,j].diff(q[i]) + D[k,i].diff(q[j]) - D[i,j].diff(q[k]))
+                print(f"c{i}{j}{k} = {c}")
+
+def equations_of_motion(links):
+    D = inertia_matrix(links)
+    C = christoffel(D)
+
+def kinetic_energy(links: tp.List[Link]):
     q = to_q(links)
     dq = q.diff(t)
     Dq = inertia_matrix(links)
     K = 0.5 * dq.T * Dq * dq
+    return K
 
-kinetic_enegy(links)
+def potential_energy(links: tp.List[Link]):
+    q = to_q(links)
+    c0 = sp.Matrix([
+        links[0].l/2 * sp.cos(q[0]),
+        links[0].l/2 * sp.sin(q[0]),
+        0.0
+    ])
+    c1 = sp.Matrix([
+        links[0].l * sp.cos(q[0]) + links[1].l/2 * sp.cos(q[0] + q[1]),
+        links[0].l * sp.sin(q[0]) + links[1].l/2 * sp.sin(q[0] + q[1]),
+        0.0
+    ])
+    g = sp.Matrix([0.0, -9.82, 0.0])
+    P0 = g.T * c0 * links[0].m
+    P1 = g.T * c1 * links[1].m
+    P = P0 + P1
+    return P
+
+DCg = equations_of_motion(links)
+
+print("asd")
